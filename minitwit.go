e715cd1f6bc5bb0fb049fedfeaea78ead14e5a08
jepli <jepli@itu.dk>
2023-02-07 18:15:50 +0100

    Database properly connected
    Co-authored-by: Benjamin Hammer Thygesen <beth@itu.dk>
    Co-authored-by: Daniel Kjellberg <dakj@itu.dk>
    Co-authored-by: Johan Flensmark <jokf@itu.dk>
    Co-authored-by: Nikolaj Sørensen <nikso@itu.dk>

--- minitwit.go
+++ minitwit.go
@@ -4,10 +4,14 @@ import "github.com/gin-gonic/gin"
 import "net/http"
 import "log"
 import "database/sql"
+import _ "github.com/mattn/go-sqlite3"
+import "fmt"
 
+//global database variable
+var DB *sql.DB
 
 // Configuration
-const DATABASE = "itu-minitwit.db"
+const DATABASE = "/tmp/minitwit.db"
 
 func setupRouter() *gin.Engine {
 	router := gin.Default();
@@ -22,22 +26,29 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
+	connect_db()
+	id := get_user_id("Leonora")
+	fmt.Println("id: " + id)
 	r := setupRouter()
 	r.Run(":8080")
 }
 
 
 func connect_db() *sql.DB {
-	//os.Remove(DATABASE)
-
 	db, err := sql.Open("sqlite3", DATABASE)
-
-	if (err != nil) {
+	if err != nil {
 		log.Fatal(err)
 	}
-	return db;
+	
+	DB = db
+	return db
 }
 
-func get_user_id(username) {
-	
+func get_user_id(username string) string {
+	var id string
+	err := DB.QueryRow("select user_id from user where username = ?", username).Scan(&id)
+	if err != nil { 
+		log.Fatal(err)
+	}
+	return id
 }
