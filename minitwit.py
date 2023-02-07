f99d71b298f744955895c22759857933205ce613
jepli <jepli@itu.dk>
2023-02-07 17:07:04 +0100

    Refactoring minitwit_tests
    Co-authored-by: Johan Flensmark <jokf@itu.dk>
    Co-authored-by: Daniel Kjellberg <dakj@itu.dk>
    Co-authored-by: Nikolaj Sørensen <nikso@itu.dk>
    Co-authored-by: Benjamin Hammer Thygesen <beth@itu.dk>

--- minitwit.py
+++ minitwit.py
@@ -38,7 +38,7 @@ def connect_db():
 def init_db():
     """Creates the database tables."""
     with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
+        with app.open_resource('schema.sql','r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
