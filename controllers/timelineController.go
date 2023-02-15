5b6919423bfcdef2399b5ceed0580f93ae2e80ba
Johan Flensmark <jokf@itu.dk>
2023-02-15 12:52:54 +0100

    split timeline routes

--- controllers/timelineController.go
+++ controllers/timelineController.go
@@ -13,34 +13,35 @@ import (
 
 const PAGE_SIZE = 30
 
-func PublicTimelineHandler() gin.HandlerFunc {
+func UserTimelineHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		session := sessions.Default(c)
-		user := session.Get(globals.Userkey)
+		user := session.Get(globals.Userkey).(model.User)
 		db := helpers.GetTypedDb(c)
-		userProfileName := c.Param("username")
 
+		userProfileName := c.Param("username")
+		following := false
+		fmt.Print(userProfileName)
 		var profiles =  []model.User{}
 		db.Select(&profiles,`select * from user where username = ?`, userProfileName);
+	
 
-		if(len(profiles) <= 0) {
+		if(!helpers.RequestedUserExists(userProfileName, profiles)) {
 			 c.JSON(http.StatusNotFound, gin.H{"error": "User does not exist"})
 			 return
-		}
-		
-
-		fmt.Print("the requested user is:" + userProfileName);
+		} 
+	
 		entries := []model.TimelineMessage{}
 		db.Select(&entries, `select message.*, user.* from message, user
-        where message.flagged = 0 and message.author_id = user.user_id
-        order by message.pub_date desc limit ?`, PAGE_SIZE)
+        where message.flagged = 0 and message.author_id = ?
+        order by message.pub_date desc limit ?`, profiles[0].UserId, PAGE_SIZE)
 		// user timeline
 		// gin.H should contain a title text + user object
 		c.HTML(http.StatusOK, "timeline.html", gin.H{
 			"user":         user,
-			"user_profile": nil,
-			"followed":     false,
-			"isSelf":       false,
+			"user_profile": profiles[0],
+			"followed":     following,
+			"isSelf":       profiles[0].UserId == user.UserId,
 			"messages":     entries,
 			"title":        "Timeline",
 		})
@@ -48,6 +49,27 @@ func PublicTimelineHandler() gin.HandlerFunc {
 		// should contain a user_profile object for a user timeline
 		// should contain whether following or not
 		// should contain whether it is yourself
+	}
+}
+
+
+func PublicTimelineHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	db := helpers.GetTypedDb(c)
+	entries := []model.TimelineMessage{}
+		db.Select(&entries, `select message.*, user.* from message, user
+        where message.flagged = 0 and message.author_id = user.user_id
+        order by message.pub_date desc limit ?`, PAGE_SIZE)
+		// user timeline
+		// gin.H should contain a title text + user object
+		c.HTML(http.StatusOK, "timeline.html", gin.H{
+			"user":         nil,
+			"user_profile": nil,
+			"followed":     false,
+			"isSelf":       false,
+			"messages":     entries,
+			"title":        "Timeline",
+		})
 	}
 }
 
--- routes/routes.go
+++ routes/routes.go
@@ -12,7 +12,7 @@ func PublicRoutes(g *gin.RouterGroup) {
 	g.GET("/register", controllers.RegisterGetHandler())
 	g.POST("/register", controllers.RegisterPostHandler())
 	g.GET("/public", controllers.PublicTimelineHandler())
-	g.GET("/:username", controllers.PublicTimelineHandler())
+	g.GET("/:username", controllers.UserTimelineHandler())
 }
 
 func PrivateRoutes(g *gin.RouterGroup) {
