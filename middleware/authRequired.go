e3d50184a7c3dcc3c8e4219212d88690a055a1fc
Johan Flensmark <jokf@itu.dk>
2023-02-21 14:43:06 +0100

    Create simulator middleware

--- middleware/authRequired.go
+++ middleware/authRequired.go
@@ -8,6 +8,16 @@ import (
 	"net/http"
 )
 
+func CheckRequestFromSimulator(c *gin.Context){
+	header := c.GetHeader("Authorization")
+	if(header != "Basic c2ltdWxhdG9yOnN1cGVyX3NhZmUh") {
+		c.JSON(http.StatusNotFound, gin.H{"error": "You are not authorized to use this resource!"})
+		c.Abort()
+		return
+	}
+	c.Next()
+}
+
 func AuthRequired(c *gin.Context) {
 	session := sessions.Default(c)
 	user := session.Get("user")
