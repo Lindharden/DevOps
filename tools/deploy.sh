be693c97f3929be40ca39b4b8e1d45375cf71163
jepli <jepli@itu.dk>
2023-02-24 12:49:58 +0100

    deploy.sh removed hyphens in docker command

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,6 +1,4 @@
-source ~/.bash_profile
-
 cd /vagrant
 
-docker-compose -f docker-compose.yml pull
-docker-compose -f docker-compose.yml up -d
+docker compose -f docker-compose.yml pull
+docker compose -f docker-compose.yml up -d
